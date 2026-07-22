e2f2ac68d119e547733dba89fc567857a76829e1
eaindranaing <45616806+ooenlhw@users.noreply.github.com>
2026-07-22 19:47:12 +0630

    chore: align prisma table mappings

--- prisma/migrations/20260719000000_add_user_table/migration.sql
+++ prisma/migrations/20260719000000_add_user_table/migration.sql
@@ -1,16 +1,16 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(255) NOT NULL,
     "password_hash" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "streak_count" INTEGER NOT NULL DEFAULT 0,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
