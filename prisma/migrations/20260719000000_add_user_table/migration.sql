d8ac6aab5450db7c16608bbc7920a83fa06f814f
eaindranaing <45616806+ooenlhw@users.noreply.github.com>
2026-07-22 19:34:30 +0630

    fix: remove map to be consistent

--- prisma/migrations/20260719000000_add_user_table/migration.sql
+++ prisma/migrations/20260719000000_add_user_table/migration.sql
@@ -1,16 +1,16 @@
 -- CreateTable
-CREATE TABLE "user" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(255) NOT NULL,
     "password_hash" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "streak_count" INTEGER NOT NULL DEFAULT 0,
 
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
