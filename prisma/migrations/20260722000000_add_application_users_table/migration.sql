02620d089553371e020352b3883c1bebdd646091
eaindranaing <eaindranaing026@gmail.com>
2026-07-23 20:16:04 +0630

    chore: rename application user join table

--- prisma/migrations/20260722000000_add_application_users_table/migration.sql
+++ prisma/migrations/20260722000000_add_application_users_table/migration.sql
@@ -1,23 +1,23 @@
 -- CreateTable
-CREATE TABLE "application_users" (
+CREATE TABLE "application_user" (
     "id" SERIAL NOT NULL,
     "application_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
 
-    CONSTRAINT "application_users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "application_user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "application_users_application_id_user_id_key" ON "application_users"("application_id", "user_id");
+CREATE UNIQUE INDEX "application_user_application_id_user_id_key" ON "application_user"("application_id", "user_id");
 
 -- CreateIndex
-CREATE INDEX "application_users_application_id_idx" ON "application_users"("application_id");
+CREATE INDEX "application_user_application_id_idx" ON "application_user"("application_id");
 
 -- CreateIndex
-CREATE INDEX "application_users_user_id_idx" ON "application_users"("user_id");
+CREATE INDEX "application_user_user_id_idx" ON "application_user"("user_id");
 
 -- AddForeignKey
-ALTER TABLE "application_users" ADD CONSTRAINT "application_users_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "application_user" ADD CONSTRAINT "application_user_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "application_users" ADD CONSTRAINT "application_users_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "application_user" ADD CONSTRAINT "application_user_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
